2f60b9541e0768e8cfc596800f4d1105fefd2879
Esther Kim <jabbukka@naver.com>
2021-07-05 16:10:01 +0900

    added build_path parameter to render.sh

--- .github/workflows/render.sh
+++ .github/workflows/render.sh
@@ -4,9 +4,14 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 APP_NAME=$1
+BUILD_PATH=/$APP_NAME/base
+
+if [ $# -eq 2 ]; then
+  BUILD_PATH=/$APP_NAME/$2
+fi
 
 echo "Start to build for $APP_NAME"
-docker run --rm -i -v $(pwd)/$APP_NAME:/$APP_NAME --name kustomize-build-$APP_NAME sktdev/decapod-kustomize:latest kustomize build --enable_alpha_plugins /$APP_NAME/base -o $APP_NAME-manifest.yaml
+docker run --rm -i -v $(pwd)/$APP_NAME:/$APP_NAME --name kustomize-build-$APP_NAME sktdev/decapod-kustomize:latest kustomize build --enable_alpha_plugins $BUILD_PATH -o $APP_NAME-manifest.yaml
 build_result=$?
 
 if [ $build_result != 0 ]; then
